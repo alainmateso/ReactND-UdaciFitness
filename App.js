1cd07b18589e165f20c43505cd3e887addefb936
codeAl <alainmateso@gmail.com>
2020-07-15 16:09:27 +0200

    Add getMetricMetaInfo

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import AddEntry from './components/AddEntry'
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View>
+      <AddEntry />
     </View>
   );
 }
